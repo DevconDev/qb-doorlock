a193334ead3ab8e98d2d175fcd031f479c008dda
Devcon <devon.hopkins333@gmail.com>
2022-04-22 23:11:43 -0300

    created ambulance doors

---
+++ configs/ambulancedoor.lua
@@ -0,0 +1,197 @@
+
+
+-- lounge created by Devcon
+Config.DoorList['ambulancedoor-lounge'] = {
+    objCoords = vec3(-442.588989, -317.056427, 35.066193),
+    doorType = 'door',
+    doorLabel = 'Staff Lounge',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 1,
+    locked = true,
+    objYaw = 112.0,
+    objName = 493136920,
+    fixText = false,
+}
+
+-- closet created by Devcon
+Config.DoorList['ambulancedoor-closet'] = {
+    objCoords = vec3(-451.474609, -309.065826, 35.066322),
+    doorType = 'door',
+    doorLabel = 'Staff Closet',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 112.0,
+    objName = 493136920,
+    fixText = false,
+}
+
+-- diagn created by Devcon
+Config.DoorList['ambulancedoor-diagn'] = {
+    objCoords = vec3(-449.657196, -299.838287, 35.070709),
+    doorType = 'door',
+    doorLabel = 'Diagnostics',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 292.0,
+    objName = 493136920,
+    fixText = false,
+}
+
+-- lab created by Devcon
+Config.DoorList['ambulancedoor-lab'] = {
+    doorType = 'double',
+    doorLabel = 'Labratory',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    doors = {
+        {objName = 665455406, objYaw = 21.71238899231, objCoords = vec3(-458.220337, -289.258362, -130.713760)},
+        {objName = 665455406, objYaw = 201.71244812012, objCoords = vec3(-455.811310, -288.299072, -130.713760)}
+    },
+    distance = 2,
+    locked = true,
+}
+
+-- landry created by Devcon
+Config.DoorList['ambulancedoor-landry'] = {
+    objCoords = vec3(-457.839478, -293.568329, -130.723679),
+    doorType = 'door',
+    doorLabel = 'Landry',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 112.48490905762,
+    objName = 493136920,
+    fixText = false,
+}
+
+-- jan created by Devcon
+Config.DoorList['ambulancedoor-jan'] = {
+    objCoords = vec3(-454.972382, -300.554352, -130.723679),
+    doorType = 'door',
+    doorLabel = 'Janitor',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 112.33148956299,
+    objName = 493136920,
+    fixText = false,
+}
+
+-- diag2 created by Devcon
+Config.DoorList['ambulancedoor-diag2'] = {
+    objCoords = vec3(-449.174469, -301.036163, -130.719299),
+    doorType = 'door',
+    doorLabel = 'Diagnosticstwo',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 112.36657714844,
+    objName = 493136920,
+    fixText = false,
+}
+
+-- Chief created by Devcon
+Config.DoorList['ambulancedoor-Chief'] = {
+    objCoords = vec3(-502.818451, -302.207062, 69.679253),
+    doorType = 'door',
+    doorLabel = 'Chief Office',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 202.6000213623,
+    objName = -551608542,
+    fixText = false,
+}
+
+-- board created by Devcon
+Config.DoorList['ambulancedoor-board'] = {
+    objCoords = vec3(-501.997589, -312.557831, 69.679253),
+    doorType = 'door',
+    doorLabel = 'Board Room',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 112.69998931885,
+    objName = -551608542,
+    fixText = false,
+}
+
+-- admin created by Devcon
+Config.DoorList['ambulancedoor-admin'] = {
+    objCoords = vec3(-500.414276, -324.591003, 69.679253),
+    doorType = 'door',
+    doorLabel = 'Administration',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 262.60003662109,
+    objName = -551608542,
+    fixText = false,
+}
+
+-- assistant created by Devcon
+Config.DoorList['ambulancedoor-assistant'] = {
+    objCoords = vec3(-500.345673, -336.497101, 69.679253),
+    doorType = 'door',
+    doorLabel = 'Management',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 127.40000152588,
+    objName = -551608542,
+    fixText = false,
+}
+
+-- adminwing created by Devcon
+Config.DoorList['ambulancedoor-adminwing'] = {
+    doorType = 'double',
+    doorLabel = 'Admin Wing',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    doors = {
+        {objName = 665455406, objYaw = 262.0, objCoords = vec3(-495.657135, -332.209869, 69.679253)},
+        {objName = 665455406, objYaw = 81.999969482422, objCoords = vec3(-496.018127, -334.778290, 69.679253)}
+    },
+    distance = 2,
+    locked = true,
+}
+
+-- adminwingtwo created by Devcon
+Config.DoorList['ambulancedoor-adminwingtwo'] = {
+    doorType = 'double',
+    doorLabel = 'Offices',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    doors = {
+        {objName = 873979204, objYaw = 4.7000408172607, objCoords = vec3(-501.889496, -325.353668, 42.469830)},
+        {objName = 873979204, objYaw = 184.70001220703, objCoords = vec3(-499.297729, -325.140594, 42.469830)}
+    },
+    distance = 2,
+    locked = true,
+}
+
+-- reception created by Devcon
+Config.DoorList['ambulancedoor-reception'] = {
+    objCoords = vec3(-440.643921, -321.795441, 35.066833),
+    doorType = 'door',
+    doorLabel = 'Reception',
+    doorRate = 1.0,
+    authorizedJobs = { ['ambulance'] = 0 },
+    distance = 2,
+    locked = true,
+    objYaw = 293.00003051758,
+    objName = 493136920,
+    fixText = false,
+}
