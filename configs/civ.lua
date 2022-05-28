67c6b3a7333b9519285d3e33924cdbb1aa0e8ffb
Devcon <devcononegaming@gmail.com>
2022-05-28 15:49:00 -0300

    Created civ config
    will contain random doorlocks for now.
    
    - added trucker office locks

---
+++ configs/civ.lua
@@ -0,0 +1,15 @@
+
+
+-- truckeroffice created by Devcon
+Config.DoorList['civ-truckeroffice'] = {
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(-63.310394, -2519.129150, 7.545235),
+    objName = -684382235,
+    distance = 2,
+    locked = true,
+    objYaw = 325.0,
+    authorizedJobs = { ['trucker'] = 0 },
+    doorType = 'door',
+    doorLabel = 'Trucker Office',
+}
