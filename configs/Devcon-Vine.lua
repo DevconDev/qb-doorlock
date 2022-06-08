d801ffd8cc8c041d79273041091487ce727916da
Devcon <devcononegaming@gmail.com>
2022-06-08 20:39:24 -0300

    added doorlocks

---
+++ configs/Devcon-Vine.lua
@@ -0,0 +1,32 @@
+
+
+-- Devcon-Vine created by Devcon
+Config.DoorList['Devcon-Vine-Devcon-Vine'] = {
+    distance = 2,
+    cantUnlock = false,
+    doorType = 'door',
+    objName = 308207762,
+    fixText = false,
+    hideLabel = true,
+    doorRate = 1.0,
+    objCoords = vec3(7.518358, 539.526855, 176.177643),
+    authorizedCitizenIDs = { ['WTG50632'] = true },
+    doorLabel = 'Devcon-Vine',
+    objYaw = 149.99998474121,
+    locked = false,
+}
+
+-- garage created by Devcon
+Config.DoorList['Devcon-Vine-garage'] = {
+    doorType = 'garage',
+    locked = true,
+    objCoords = vec3(18.650375, 546.340149, 176.344757),
+    distance = 5,
+    doorLabel = 'Garage',
+    authorizedCitizenIDs = { ['WTG50632'] = true },
+    doorRate = 1.0,
+    fixText = false,
+    objName = 2052512905,
+    objYaw = 241.13833618164,
+    hideLabel = true,
+}
---
+++ configs/happy_shark.lua
@@ -0,0 +1,15 @@
+
+
+-- main created by Devcon
+Config.DoorList['happy_shark-main'] = {
+    objYaw = 0.0,
+    doorRate = 1.0,
+    objCoords = vec3(376.356354, -966.609741, 29.861238),
+    doorLabel = 'Main',
+    distance = 2,
+    objName = -1045015371,
+    doorType = 'door',
+    fixText = false,
+    locked = true,
+    authorizedCitizenIDs = { ['WTG50632'] = true },
+}
