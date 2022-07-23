4abb2a7073b76f8bb35d9c614d8dbd2144436a62
DevconDev <devon.hopkins333@gmail.com>
2022-07-23 16:32:06 -0300

    Added happy_shark configs

--- configs/happy_shark.lua
+++ configs/happy_shark.lua
@@ -13,3 +13,31 @@ Config.DoorList['happy_shark-main'] = {
     locked = true,
     authorizedCitizenIDs = { ['WTG50632'] = true },
 }
+
+-- Staff created by Devcon
+Config.DoorList['happy_shark-Staff'] = {
+    distance = 2,
+    authorizedCitizenIDs = { ['WTG50632'] = true },
+    doorLabel = 'Staff Only',
+    objName = -1922281023,
+    fixText = false,
+    doorType = 'door',
+    objCoords = vec3(378.256165, -969.360840, 29.761395),
+    locked = true,
+    doorRate = 1.0,
+    objYaw = 0.0,
+}
+
+-- Boss created by Devcon
+Config.DoorList['happy_shark-Boss'] = {
+    objName = -1922281023,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['happyshark'] = 0 },
+    locked = true,
+    doorType = 'door',
+    objYaw = 0.0,
+    objCoords = vec3(378.560120, -976.125061, 29.772223),
+    doorRate = 1.0,
+    doorLabel = 'Boss',
+}
